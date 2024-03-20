b052868bbfa4e6146d9ffb9e1cfc1b7d49e187cb
ArcBLR <arc11041977@gmail.com>
2024-03-20 22:26:57 +0300

    Небольшие исправления

--- src/HomeWork_4_2/MassivBubblSort.java
+++ src/HomeWork_4_2/MassivBubblSort.java
@@ -30,12 +30,13 @@ public class MassivBubblSort {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите два числа разделенные пробелом: ");
         String next = scanner.nextLine();
+        scanner.close();
         String[] s = next.split(" ");
         int i1 = Integer.valueOf(s[0]);
         int i2 = Integer.valueOf(s[1]);
         System.out.println(i1);
         System.out.println(i2);
-        for (int r = i1; r < i2; r++){
+        for (int r = i1; r <=i2; r++){
             System.out.print(" " + massivBS[r]);
         }
     }
