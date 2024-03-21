414df4c7dde4c53d858835725439c06c57ee9421
ArcBLR <arc11041977@gmail.com>
2024-03-21 10:05:10 +0300

    Небольшие исправления

--- src/HomeWork_4_2/MassivBubblSort.java
+++ src/HomeWork_4_2/MassivBubblSort.java
@@ -1,6 +1,5 @@
 package HomeWork_4_2;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class MassivBubblSort {
@@ -30,12 +29,13 @@ public class MassivBubblSort {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите два числа разделенные пробелом: ");
         String next = scanner.nextLine();
+        scanner.close();
         String[] s = next.split(" ");
         int i1 = Integer.valueOf(s[0]);
         int i2 = Integer.valueOf(s[1]);
         System.out.println(i1);
         System.out.println(i2);
-        for (int r = i1; r < i2; r++){
+        for (int r = i1; r <=i2; r++){
             System.out.print(" " + massivBS[r]);
         }
     }
---
+++ src/HomeWork_4_3/ListArray.java
@@ -0,0 +1,4 @@
+package HomeWork_4_3;
+
+public class ListArray {
+}
