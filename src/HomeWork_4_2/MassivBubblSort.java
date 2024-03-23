093440b0cf8c25ded26c36fd226184cb371bbefa
ArcBLR <arc11041977@gmail.com>
2024-03-23 18:49:45 +0300

    небольшие косметические правки + добавил ДЗ_3

--- src/HomeWork_4_2/MassivBubblSort.java
+++ src/HomeWork_4_2/MassivBubblSort.java
@@ -15,7 +15,6 @@ public class MassivBubblSort {
             for (int i = 0; i < massivBS.length - 1; i++) {
                 if (massivBS[i] > massivBS[i + 1]) {
                     isSorted = false;
-
                     buffers = massivBS[i];
                     massivBS[i] = massivBS[i + 1];
                     massivBS[i + 1] = buffers;
