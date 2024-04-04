02c91f10f39256b769b367b887ea5da1646d8ceb
ArcBLR <arc11041977@gmail.com>
2024-04-04 19:23:05 +0300

    переделал ДЗ_4_3

---
+++ src/HomeWork_4_3/MassList.java
@@ -0,0 +1,72 @@
+package HomeWork_4_3;
+
+import java.util.Objects;
+
+public class MassList<E> {
+    private Object[] data;
+    private int size;
+
+    public MassList() {
+        data = new Object[10];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(E element) {
+        ensureCapacity(size + 1);
+        data[size++] = element;
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity - data.length > 0) {
+            int newSize = Math.max(data.length * 2, minCapacity);
+            Object[] newData = new Object[newSize];
+            for (int i = 0; i < size; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+    }
+
+    public void add(int index, E element) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        ensureCapacity(size + 1);
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = element;
+        size++;
+    }
+
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (E) data[index];
+    }
+
+    public E remove(int index) {
+        Objects.checkIndex(index, size);
+        @SuppressWarnings("unchecked") E oldValue = (E) data[index];
+        fastRemove(data, index);
+        return oldValue;
+    }
+
+    private void fastRemove(Object[] es, int i) {
+        final int newSize;
+        if ((newSize = size - 1) > i)
+            System.arraycopy(es, i + 1, es, i, newSize - i);
+        es[size = newSize] = null;
+    }
+
+    public void clear() {
+        for (int to = size, i = size = 0; i < to; i++)
+            data[i] = null;
+    }
+}
+
---
+++ src/HomeWork_4_3/MyArrayList.java
@@ -0,0 +1,53 @@
+package HomeWork_4_3;
+
+public class MyArrayList {
+    static String[] newMassive = new String[]{"NewElem_100", "NewElem_101", "NewElem_102", "NewElem_103", "NewElem_104"};
+
+    public static void main(String[] args) {
+        String ds = "Длинна списка ";
+        MassList<String> list = new MassList<>();
+        System.out.println("Наполняем список элементами");
+        for (int i = 0; i < 6; i++) {
+            list.add("Ellement_" + i);
+        }
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(j));
+        }
+        System.out.println(ds + list.size());
+
+        System.out.println("Добавлякм массив \"newMassive\" к нашему списку");
+        for (int i = 0; i < newMassive.length; i++) {
+            list.add(newMassive[i]);
+        }
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(j));
+        }
+        System.out.println(ds + list.size());
+
+        System.out.println("Добавляем один элемент");
+        list.add("Ellement_6_6");
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(j));
+        }
+        System.out.println(ds + list.size());
+
+        System.out.println("Добавляем в список элемент с индексом 6");
+        list.add(6, "Ellement_5_5");
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(j));
+        }
+        System.out.println(ds + list.size());
+
+        System.out.println("Удаляем из списка элемент с индексом 5");
+        list.remove(5);
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(j));
+        }
+        System.out.println(ds + list.size());
+
+        System.out.println("Очищаем список");
+        list.clear();
+        System.out.println(ds + list.size());
+    }
+}
+
